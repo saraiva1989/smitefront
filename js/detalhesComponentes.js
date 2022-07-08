9f4112b31949ceca9815508a5734a6eef7a591ed
Daniel Saraiva <danniel.saraiva@gmail.com>
2022-07-08 15:14:46 -0300

    Ajuste para usar api netcore

--- js/detalhesComponentes.js
+++ js/detalhesComponentes.js
@@ -3,10 +3,10 @@ function componentDetalheBanner(data) {
     return `
             <div class="banner">
                 <div class="banner-img"
-                    style="background-image: url('${data.UrlBanner}');">
+                    style="background-image: url('${data.urlBanner}');">
                     <div class="nome">
-                        <h1><b>${data.Nome}</b></h1>
-                        <p>${data.Titulo}</p>
+                        <h1><b>${data.nome}</b></h1>
+                        <p>${data.titulo}</p>
                     </div>
                 </div>
             </div>
@@ -18,18 +18,18 @@ function componentDetalheTipo(element) {
             <div class="card-detalhe flex">
                 <div class="item">
                     <h1>Classe</h1>
-                    <p><img src="${imagemClasse(element.Cultura)}" height="100px"></p>
-                    <p>${element.Classe}</p>
+                    <p><img src="${imagemClasse(element.cultura)}" height="100px"></p>
+                    <p>${element.classe}</p>
                 </div>
                 <div class="item">
                     <h1>Cultura</h1>
-                    <p><img src="${imagemCultura(element.Cultura)}" height="100px"></p>
-                    <p>${element.Cultura}</p>
+                    <p><img src="${imagemCultura(element.cultura)}" height="100px"></p>
+                    <p>${element.cultura}</p>
                 </div>
                 <div class="item">
                     <h1>Tipo</h1>
                     <p><img src="img/tipo.png" height="100px"></p>
-                    <p>${element.Tipo}</p>
+                    <p>${element.tipo}</p>
                 </div>
             </div>
     `
@@ -42,8 +42,8 @@ function componentHistoria(data) {
                     <h1>História</h1>
                 </div>
                 <hr>
-                <div class="embed-responsive"><iframe src="https://www.youtube.com/embed/${data.UrlVideoHistoria}?showinfo=0" allowfullscreen></iframe></div>
-                <div class="conteudo-detalhe">${data.Historia.replaceAll('\\n', '<br>').replaceAll('"', '')}</div>
+                <div class="embed-responsive"><iframe src="https://www.youtube.com/embed/${data.urlVideoHistoria}?showinfo=0" allowfullscreen></iframe></div>
+                <div class="conteudo-detalhe">${data.historia.replaceAll('\\n', '<br>').replaceAll('"', '')}</div>
             </div>
     `
 }
@@ -89,18 +89,18 @@ function componentTabelaHabilidade(data) {
         linha += `
                 <tr>
                     <td>
-                        <div><img src="${element.Imagem}"
+                        <div><img src="${element.imagem}"
                                 alt="" srcset=""></div>
                     </td>
-                    <td>${element.Nome}</td>
-                    <td class="descricao-habilidade">${element.Descricao}</td>
+                    <td>${element.nome}</td>
+                    <td class="descricao-habilidade">${element.descricao}</td>
                     <td>
-                        ${componentListaHtml(element.InfoPoderHabilidades)}
+                        ${componentListaHtml(element.infoPoderHabilidades)}
                     </td>
                     <td>
-                        ${componentListaHtml(element.InfoHabilidades)}
+                        ${componentListaHtml(element.infoHabilidades)}
                     </td>
-                    <td><button type="button" class="btn-video" onclick="componentModalHabilidade('${element.UrlVideo}', 500, 700)"><span>
+                    <td><button type="button" class="btn-video" onclick="componentModalHabilidade('${element.urlVideo}', 500, 700)"><span>
                                 Video Habilidade
                             </span></button></td>
                 </tr>
@@ -120,19 +120,19 @@ function componentTabelaHabilidadeMobile(data) {
                 <div>
                     <h1>Aspectos Mortíferos</h1>
                     <p><img
-                            src="${element.Imagem}" alt=""
+                            src="${element.imagem}" alt=""
                             srcset=""></p>
-                    <div class="historia"><b>Descrição Habilidade: </b>${element.Descricao}</div> 
+                    <div class="historia"><b>Descrição Habilidade: </b>${element.descricao}</div> 
                     <br> 
                     <b><p class="destaque-habilidade">Dano da habilidade:</p></b>
-                        ${componentListaHtml(element.InfoPoderHabilidades)}
+                        ${componentListaHtml(element.infoPoderHabilidades)}
                      <br> 
                      <b><p class="destaque-habilidade">informação da habilidade:</p></b>
-                        ${componentListaHtml(element.InfoHabilidades)}
+                        ${componentListaHtml(element.infoHabilidades)}
                     <br>
                     <p style="text-align: center;">
                     <button type="button"
-                            class="btn-video" onclick="componentModalHabilidade('${element.UrlVideo}', 300, 350)">
+                            class="btn-video" onclick="componentModalHabilidade('${element.urlVideo}', 300, 350)">
                             <span>Video Habilidade</span>
                     </button></p> 
                     <br>
@@ -160,7 +160,7 @@ function componentListaHtml(data) {
     data.forEach(element => {
         ul += `
         <ul>
-            <li><b>${element.Descricao}</b> ${element.Valor}</li>
+            <li><b>${element.descricao}</b> ${element.valor}</li>
         </ul>
         ` 
     });
@@ -176,17 +176,17 @@ function componentSkin(data) {
                     <div id="card-deus" class="card-deus">
                         <div class="avatar">
                             <div class="avatar-background" alt="Avatar"
-                                style=" background-image: url('${element.Imagem}');">
+                                style=" background-image: url('${element.imagem}');">
                                 <div class="nome">
-                                    <h1><b>${element.Nome}</b></h4>
+                                    <h1><b>${element.nome}</b></h4>
                                 </div>
                             </div>
                         </div>
 
                         <div class="">
-                            <p>${element.Tipo}</p>
-                            <p><b>Favor: </b>${element.Favor}</p>
-                            <p><b>Gema: </b>${element.Gema}</p>
+                            <p>${element.tipo}</p>
+                            <p><b>Favor: </b>${element.favor}</p>
+                            <p><b>Gema: </b>${element.gema}</p>
                         </div>
                     </div>
                 
